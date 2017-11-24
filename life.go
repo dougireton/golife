d01b0c1b23585d31a4bd8f542d968e022ae1e3c8
Doug Ireton <doug@1strategy.com>
2017-11-24 12:20:13 -0800

    Add Show and Seed methods

--- life.go
+++ life.go
@@ -1,15 +1,19 @@
 package main
 
-import "fmt"
-
-const (
-	x = 80
-	y = 15
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 type Universe [][]bool
 
-func NewUniverse() Universe {
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func NewUniverse(x, y int) Universe {
 	u := make([][]bool, y)
 	cells := make([]bool, x*y)
 	for row := range u {
@@ -18,8 +22,53 @@ func NewUniverse() Universe {
 	return u
 }
 
-func main() {
+// formatRow formats a row in the Universe.
+// Helper function for the Show function.
+func formatRow(r []bool) string {
+	var b bytes.Buffer
+	for _, cell := range r {
+		if cell {
+			b.WriteString("*")
+		} else {
+			b.WriteString(" ")
+		}
+	}
+	b.WriteString("\n")
+	return b.String()
+}
 
-	foo := NewUniverse()
-	fmt.Println(foo)
+// Show creates a printable universe.
+// Live cells are shown with an asterisk and dead cells
+// are shown with a space.
+func (u Universe) Show() string {
+	var b bytes.Buffer
+	for _, row := range u {
+		b.WriteString(formatRow(row))
+	}
+	return b.String()
+}
+
+func initializeCell(p float64) bool {
+	num := rand.Intn(100) + 1
+	return num <= int(p*100)
+}
+
+// Seed randomly sets p % of the cells to alive (true).
+// For a 1200 cell universe with p = 25%, the number of live cells should be roughly 300.
+func (u Universe) Seed(p float64) {
+	for i, row := range u {
+		for j := range row {
+			u[i][j] = initializeCell(p)
+		}
+	}
+}
+
+// Alive returns true if the cell at position x, y is alive, false if dead.
+func (u Universe) Alive(x, y int) bool {
+	return true
+}
+
+func main() {
+	foo := NewUniverse(80, 15)
+	fmt.Print(foo.Show())
 }
--- life_test.go
+++ life_test.go
@@ -1,11 +1,25 @@
 package main
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
+
+var fmtRowTests = []struct {
+	row  []bool
+	want string
+}{
+	{[]bool{false, true, false}, " * \n"},
+	{[]bool{true, false, true}, "* *\n"},
+}
 
 // Test NewUniverse function that uses make to allocate and return a Universe
 // with Height rows and Width columns per row.
 func TestNewUniverse(t *testing.T) {
-	u := NewUniverse()
+	t.Parallel()
+	x, y := 80, 15
+
+	u := NewUniverse(x, y)
 	if len(u) != y {
 		t.Errorf("Newly initialized universe is %v rows high. Want %v rows.", len(u), y)
 	}
@@ -15,4 +29,63 @@ func TestNewUniverse(t *testing.T) {
 			t.Errorf("Newly initialized universe is %v columns wide. Want %v columns.", len(u[0]), x)
 		}
 	}
+}
+
+func TestFormatRow(t *testing.T) {
+	t.Parallel()
+
+	for _, tt := range fmtRowTests {
+		got := formatRow(tt.row)
+		if got != tt.want {
+			t.Errorf("formatRow(%v) => %v; want %v", tt.row, got, tt.want)
+		}
+	}
+}
+
+func TestUniverseShow(t *testing.T) {
+	t.Parallel()
+	x, y := 80, 15
+
+	u := NewUniverse(x, y)
+	got := len(u.Show())
+	want := (x * y) + y
+
+	if got != want {
+		t.Errorf("NewUniverse is a string %v characters long; want %v characters long.", got, want)
+	}
+}
+
+func TestUniverseSeed(t *testing.T) {
+	t.Parallel()
+	x, y := 80, 15
+
+	rand.Seed(1)
+	percentLive := 0.25
+	totalCells := x * y
+	var liveActual int
+	liveExpected := 329 // this value depends on rand.Seed(1), totalCells = 1200, and % live of 0.25
+
+	u := NewUniverse(x, y)
+	u.Seed(percentLive)
+
+	for _, row := range u {
+		for _, cell := range row {
+			if cell {
+				liveActual++
+			}
+		}
+	}
+
+	if liveActual != liveExpected {
+		t.Errorf("After Seeding, new universe has %v/%v cells alive; want %v/%v.",
+			liveActual, totalCells, liveExpected, totalCells)
+	}
+}
+
+func TestAlive(t *testing.T) {
+	t.Parallel()
+	// x, y := 5, 5
+
+	// u := NewUniverse(x, y)
+
 }
