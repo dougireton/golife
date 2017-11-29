ed392a1492f231aa6266480edc4fd621d25e5e93
Doug Ireton <doug@1strategy.com>
2017-11-28 21:15:16 -0800

    Add Alive method

--- life.go
+++ life.go
@@ -13,6 +13,7 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
+// NewUniverse creates a new, empty Universe of y columns high by x columns wide.
 func NewUniverse(x, y int) Universe {
 	u := make([][]bool, y)
 	cells := make([]bool, x*y)
@@ -56,16 +57,42 @@ func initializeCell(p float64) bool {
 // Seed randomly sets p % of the cells to alive (true).
 // For a 1200 cell universe with p = 25%, the number of live cells should be roughly 300.
 func (u Universe) Seed(p float64) {
-	for i, row := range u {
-		for j := range row {
-			u[i][j] = initializeCell(p)
+	for y, row := range u {
+		for x := range row {
+			u[y][x] = initializeCell(p)
 		}
 	}
 }
 
 // Alive returns true if the cell at position x, y is alive, false if dead.
+//
+// The universe needs to wrap around, such that all cells, including edge
+// and corner cells, have eight neighbors. For example, given a cell, "a", in the table below,
+// its eight neighbors are shown by eight compass points (N, S, E, W, etc).
+// Therefore, for cell "a" at position (x=0, y=0), its NW neighbor is at (x=3, y=3),
+// and its W neighbor is at (x=3, y=0).
+//   +-----------------+
+// 0 | a | E  |   | W  |
+// 1 | S | SE |   | SW |
+// 2 |   |    |   |    |
+// 3 | N | NE |   | NW |
+//   + ----------------+
+//     0   1    2   3
 func (u Universe) Alive(x, y int) bool {
-	return true
+	height := len(u)
+	width := len(u[0])
+
+	// Use modulus to make the rows/colums "wrap around".
+	// See here for a simplified example: https://play.golang.org/p/q68IKzNof1
+	y = (y + height) % height
+	x = (x + width) % width
+
+	return u[y][x]
+}
+
+// Neighbors returns the number of live neighbors for a given cell, from zero to eight.
+func (u Universe) Neighbors(x, y int) int {
+	return 0
 }
 
 func main() {
--- life_test.go
+++ life_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 )
@@ -13,7 +14,34 @@ var fmtRowTests = []struct {
 	{[]bool{true, false, true}, "* *\n"},
 }
 
-// Test NewUniverse function that uses make to allocate and return a Universe
+// aliveTests assume a 4x4 universe
+var aliveTests = []struct {
+	x    int
+	y    int
+	want bool
+}{
+	// cell a
+	{0, -1, true},   // N
+	{1, -1, false},  // NE
+	{1, 0, true},    // E
+	{1, 1, false},   // SE
+	{0, 1, true},    // S
+	{-1, 1, false},  // SW
+	{-1, 0, true},   // W
+	{-1, -1, false}, // NW
+
+	// cell b
+	{3, 2, true},  // N
+	{4, 2, false}, // NE
+	{4, 3, true},  // E
+	{4, 4, false}, // SE
+	{3, 4, true},  // S
+	{2, 4, false}, // SW
+	{2, 3, true},  // W
+	{2, 2, false}, // NW
+}
+
+// Test NewUniverse function which uses make to allocate and return a Universe
 // with Height rows and Width columns per row.
 func TestNewUniverse(t *testing.T) {
 	t.Parallel()
@@ -31,6 +59,8 @@ func TestNewUniverse(t *testing.T) {
 	}
 }
 
+// TestFormatRow verifies the FormatRow helper function formats a Universe
+// row for printing to the screen
 func TestFormatRow(t *testing.T) {
 	t.Parallel()
 
@@ -57,13 +87,14 @@ func TestUniverseShow(t *testing.T) {
 
 func TestUniverseSeed(t *testing.T) {
 	t.Parallel()
-	x, y := 80, 15
 
 	rand.Seed(1)
+
+	x, y := 80, 15
+	total := x * y
 	percentLive := 0.25
-	totalCells := x * y
-	var liveActual int
-	liveExpected := 329 // this value depends on rand.Seed(1), totalCells = 1200, and % live of 0.25
+	var got int
+	want := 329 // this value depends on rand.Seed(1), total = 1200, and % live of 0.25
 
 	u := NewUniverse(x, y)
 	u.Seed(percentLive)
@@ -71,21 +102,44 @@ func TestUniverseSeed(t *testing.T) {
 	for _, row := range u {
 		for _, cell := range row {
 			if cell {
-				liveActual++
+				got++
 			}
 		}
 	}
 
-	if liveActual != liveExpected {
+	if got != want {
 		t.Errorf("After Seeding, new universe has %v/%v cells alive; want %v/%v.",
-			liveActual, totalCells, liveExpected, totalCells)
+			got, total, want, total)
 	}
 }
 
+// TestAlive verifies the Universe.Alive method.
 func TestAlive(t *testing.T) {
 	t.Parallel()
-	// x, y := 5, 5
+	x, y := 4, 4
 
-	// u := NewUniverse(x, y)
+	u := NewUniverse(x, y)
+	fmt.Println("u len:", len(u[0]))
+
+	// populate the universe
+	// Cells are initialized to false. Set specified ones to alive/true.
+	u[0][1] = true // E
+	u[0][3] = true // S
+	u[1][0] = true // S
+	u[2][3] = true // N
+	u[3][0] = true // E
+	u[3][2] = true // W
+
+	for _, tt := range aliveTests {
+		got := u.Alive(tt.x, tt.y)
+
+		if got != tt.want {
+			t.Errorf("u.Alive(%v, %v) => %v; want %v", tt.x, tt.y, got, tt.want)
+		}
+	}
+
+}
+
+func TestNeighbors(t *testing.T) {
 
 }
